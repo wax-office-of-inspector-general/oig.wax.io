0decb1b711fd3ad90f7e1bf3a79aae61f17d14c5
rakeden <jannis.migdalas@gmail.com>
2022-12-11 16:03:28 +0100

    scaffold vue3 with vite

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView } from "vue-router";
+
+import HeadingComp from "@/components/HeadingComp.vue";
+import FooterComp from "@/components/FooterComp.vue";
+</script>
+
+<template>
+  <HeadingComp />
+
+  <RouterView />
+
+  <FooterComp />
+</template>
+
+<style scoped></style>
