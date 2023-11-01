15f740f78b143a6bd4e52122ff25d212a8e1199c
Alexandre Santos <alexandressjr@gmail.com>
2023-10-31 23:55:14 -0300

    Proclaim and Nominf sequential callback actions trial and overall lint fixes

--- src/components/modal/ConfirmationModal.vue
+++ src/components/modal/ConfirmationModal.vue
@@ -13,75 +13,75 @@ const props = defineProps({
   onCancel: Function,
   title: String,
   description: String,
-  confirmLabel: {type: String, default: "Confirm"},
-  cancelLabel: {type: String, default: "Cancel"}
+  confirmLabel: { type: String, default: 'Confirm' },
+  cancelLabel: { type: String, default: 'Cancel' }
 });
 </script>
 
 <template>
-    <TransitionRoot appear :show="show" as="template">
-      <Dialog as="div" @close="onCancel" class="relative z-10">
-        <TransitionChild
-          as="template"
-          enter="duration-300 ease-out"
-          enter-from="opacity-0"
-          enter-to="opacity-100"
-          leave="duration-200 ease-in"
-          leave-from="opacity-100"
-          leave-to="opacity-0"
+  <TransitionRoot appear :show="show" as="template">
+    <Dialog as="div" @close="onCancel" class="relative z-10">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black bg-opacity-25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
         >
-          <div class="fixed inset-0 bg-black bg-opacity-25" />
-        </TransitionChild>
-
-        <div class="fixed inset-0 overflow-y-auto">
-          <div
-            class="flex min-h-full items-center justify-center p-4 text-center"
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
           >
-            <TransitionChild
-              as="template"
-              enter="duration-300 ease-out"
-              enter-from="opacity-0 scale-95"
-              enter-to="opacity-100 scale-100"
-              leave="duration-200 ease-in"
-              leave-from="opacity-100 scale-100"
-              leave-to="opacity-0 scale-95"
+            <DialogPanel
+              class="w-full max-w-2xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
             >
-              <DialogPanel
-                class="w-full max-w-2xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+              <DialogTitle
+                as="h3"
+                class="text-lg font-medium leading-6 text-primary"
               >
-                <DialogTitle
-                  as="h3"
-                  class="text-lg font-medium leading-6 text-primary"
+                {{ props.title }}
+              </DialogTitle>
+
+              <div class="py-10">
+                <h3 class="text-base font-semibold leading-6 text-font">
+                  {{ props.description }}
+                </h3>
+              </div>
+
+              <div class="mt-6 flex justify-end">
+                <button
+                  type="button"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-gray-200 px-4 py-2 text-sm font-medium text-primary hover:bg-blue-200 focus:outline-none"
+                  @click="onCancel"
                 >
-                  {{ props.title }}
-                </DialogTitle>
-
-                <div class="py-10">
-                  <h3 class="text-base font-semibold leading-6 text-font">
-                    {{ props.description }}
-                  </h3>
-                </div>
-
-                <div class="mt-6 flex justify-end">
-                  <button
-                    type="button"
-                    class="inline-flex justify-center rounded-md border border-transparent bg-gray-200 px-4 py-2 text-sm font-medium text-primary hover:bg-blue-200 focus:outline-none"
-                    @click="onCancel"
-                  >
-                    {{ props.cancelLabel }}
-                  </button>
-                  <button
-                    type="button"
-                    class="ml-4 inline-flex justify-center rounded-md border border-transparent bg-primary px-4 py-2 text-sm font-medium text-white hover:bg-primary-700 focus:outline-none"
-                    @click="onConfirm"
-                  >
-                    {{ props.confirmLabel }}
-                  </button>
-                </div>
-              </DialogPanel>
-            </TransitionChild>
-          </div>
+                  {{ props.cancelLabel }}
+                </button>
+                <button
+                  type="button"
+                  class="ml-4 inline-flex justify-center rounded-md border border-transparent bg-primary px-4 py-2 text-sm font-medium text-white hover:bg-primary-700 focus:outline-none"
+                  @click="onConfirm"
+                >
+                  {{ props.confirmLabel }}
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
         </div>
-      </Dialog>
-    </TransitionRoot>
+      </div>
+    </Dialog>
+  </TransitionRoot>
 </template>
