d769303816c8de2bb055d5d6a9fac6cdc37061a5
rakeden <5896201+rakeden@users.noreply.github.com>
2023-11-13 20:51:24 +0100

    Adding section about OIG pillars

--- src/views/TheOIGView.vue
+++ src/views/TheOIGView.vue
@@ -3,14 +3,17 @@ import SimpleHero from '../components/hero/SimpleHero.vue';
 import OIGExplanation from '../components/OIGExplanation.vue';
 import OIGElectionProcess from '../components/OIGElectionProcess.vue';
 import OIGTeam from '../components/OIGTeam.vue';
+import OIGPillars from '@/components/oig/OIGPillars.vue';
 
-const title = 'The OIG on WAX';
+const title = 'OIG, more than a framework';
 const desc =
-  '3 Inspector Generals, elected by the WAX community decentralized the WAX network.';
+  'A unique approach to blockchain governance for a complex DPos environment';
 </script>
+
 <template>
   <SimpleHero :title="title" :desc="desc" />
   <OIGExplanation />
+  <OIGPillars />
   <OIGTeam />
   <OIGElectionProcess />
 </template>
