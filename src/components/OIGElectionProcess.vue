0184502352d1421c1f19e2c2a5dfe6c083f10f13
rakeden <5896201+rakeden@users.noreply.github.com>
2023-11-13 21:15:09 +0100

    Adding more content about The OIG

--- src/components/OIGElectionProcess.vue
+++ src/components/OIGElectionProcess.vue
@@ -1,5 +1,15 @@
 <script setup>
 import ButtonText from '@/components/button/ButtonText.vue';
+import { CheckBadgeIcon } from '@heroicons/vue/24/outline';
+
+const headline = 'The Election process';
+
+const electionProcess = [
+  'Elections are held every six months, with each Inspector General serving an 18-month term.',
+  'As soon as a new IG is elected, the previous IG&#39;s tenure ends and they are rotated out.',
+  'The rotation of OIG seats takes place on the first day of February and August every year.',
+  'Nomination and Voting period is set to 7 full days each. Ideally the election concludes 3 full months before the seat is replaced.'
+]
 </script>
 <template>
   <div class="py-16 sm:py-20">
@@ -9,7 +19,7 @@ import ButtonText from '@/components/button/ButtonText.vue';
           <h2
             class="mt-2 text-2xl font-bold tracking-tight text-primary sm:text-4xl"
           >
-            IG Election Process
+            {{ headline }}
           </h2>
           <div
             class="mt-8 grid max-w-xl grid-cols-1 gap-8 gap-y-12 text-base leading-7 lg:max-w-none lg:grid-cols-2"
@@ -23,22 +33,17 @@ import ButtonText from '@/components/button/ButtonText.vue';
               <ButtonText href="/election" text="When is the next election?" />
             </div>
             <div>
-              <ul class="list-disc list-outside ml-4">
-                <li>
-                  Elections are held every six months, with each Inspector
-                  General serving an 18-month term.
-                </li>
-                <li>
-                  As soon as a new IG is elected, the previous IG’s tenure ends
-                  and they are rotated out.
-                </li>
-                <li>
-                  The rotation of OIG seats takes place on the first day of
-                  February and August every year.
-                </li>
-                <li>
-                  The OIG is compensated in WAX Tokens and their salary is paid
-                  out of the WAX Worker Proposal fund.
+              <ul role="list" class="space-y-8 text-gray-600">
+                <li
+                  v-for="(ep, index) in electionProcess"
+                  :key="index"
+                  class="flex gap-x-3"
+                >
+                  <CheckBadgeIcon
+                    class="mt-1 h-5 w-5 flex-none text-primary"
+                    aria-hidden="true"
+                  />
+                  <span v-html="ep"></span>
                 </li>
               </ul>
             </div>
