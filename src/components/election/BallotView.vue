80378f2c3bdba3276b6f2894fa4ea11a2187cc4f
rakeden <5896201+rakeden@users.noreply.github.com>
2023-11-14 16:47:39 +0100

    change BallotView layout & change BallotTimeline comp

--- src/components/election/BallotView.vue
+++ src/components/election/BallotView.vue
@@ -4,6 +4,7 @@ import BallotTimeline from './BallotTimeline.vue';
 import BallotTimelineDetails from './BallotTimelineDetails.vue';
 import BallotCandidates from './BallotCandidates.vue';
 import BallotNomination from './BallotNomination.vue';
+import OIGTeam from '@/components/OIGTeam.vue';
 </script>
 
 <template>
@@ -13,5 +14,6 @@ import BallotNomination from './BallotNomination.vue';
     <BallotNomination />
     <BallotCandidates />
     <BallotTimelineDetails />
+    <OIGTeam />
   </div>
 </template>
