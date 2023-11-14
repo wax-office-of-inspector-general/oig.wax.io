916090118e895e21381b54f83e797c27c51a55b9
Alexandre Santos <alexandressjr@gmail.com>
2023-11-14 10:27:23 -0300

    Small fixes to imports

--- src/components/election/CandidateDetails.vue
+++ src/components/election/CandidateDetails.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useStore } from 'vuex';
 import { useSession } from '../../composables/useSession';
 import {
