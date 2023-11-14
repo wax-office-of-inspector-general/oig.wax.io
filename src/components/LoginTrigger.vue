efd994ed081c2a68ae677cf3126fd6d058d05b01
rakeden <5896201+rakeden@users.noreply.github.com>
2023-11-14 15:05:52 +0100

    update wallet popover styles & add links to popover

--- src/components/LoginTrigger.vue
+++ src/components/LoginTrigger.vue
@@ -2,7 +2,6 @@
 import { useSessionKit, useSession } from '../composables/useSession';
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue';
 import { WalletIcon, ArrowLongRightIcon } from '@heroicons/vue/24/outline';
-import ButtonAction from '@/components/button/ButtonAction.vue';
 
 const { login, logout } = useSessionKit();
 const session = useSession();
@@ -13,19 +12,21 @@ const session = useSession();
     <PopoverButton
       :class="[
         session ? 'text-white' : 'text-white',
-        'group inline-flex items-center rounded-md text-sm font-medium hover:text-gray-900 focus:outline-none'
+        'group inline-flex items-center rounded-md text-sm hover:text-gray-900 focus:outline-none'
       ]"
     >
       <div
-        class="bg-primary hover:bg-primary rounded-md text-sm font-serif font-medium text-white hover:text-white"
+        class="bg-primary hover:bg-primary rounded-md text-sm text-white hover:text-white"
       >
-        <div v-if="session" class="flex flex-row items-center px-8 py-2.5">
+        <div
+          v-if="session"
+          class="min-w-[180px] justify-center flex flex-row items-center px-8 py-2.5"
+        >
           <WalletIcon
             class="mr-2 h-5 w-5 group-hover:text-white text-white"
             aria-hidden="true"
           />
-
-          <span>{{ session.actor }}</span>
+          <span class="text-sm leading-5">{{ session.actor }}</span>
         </div>
         <div
           v-else
@@ -44,15 +45,32 @@ const session = useSession();
 
     <PopoverPanel
       v-if="session"
-      class="absolute z-10 w-full flex items-stretch bg-primary"
+      class="absolute z-10 w-full drop-shadow-md bg-white rounded-b-md -translate-y-1"
     >
-      <PopoverButton class="w-full">
-        <ButtonAction
-          text="Logout"
-          :onClick="logout"
-          cssClass="text-xs w-full"
-        />
-      </PopoverButton>
+      <div class="flex flex-col items-stretch">
+        <div class="p-3  flex flex-col">
+          <p class="text-xs text-font/50">wallet connected</p>
+          <p class="text-sm">
+            <strong>{{ session.actor }}</strong>@{{ session.permission }}</p>
+        </div>
+        <div class="mt-2 flex flex-col divide-y">
+          <a
+            :href="`https://waxblock.io/account/${session.actor}`"
+            target="_blank"
+            class="relative inline-block whitespace-nowrap px-3 py-2 text-xs text-primary focus-visible:none hover:bg-primary hover:text-white"
+          >
+            View Account
+            <span class="ml-1" aria-hidden="true"> &rarr;</span>
+          </a>
+          <button
+            :onClick="logout"
+            class="relative text-left inline-block whitespace-nowrap px-3 py-2 text-xs text-primary focus-visible:none hover:bg-primary hover:text-white"
+          >
+            Logout
+            <span class="ml-1" aria-hidden="true"> &rarr;</span>
+          </button>
+        </div>
+      </div>
     </PopoverPanel>
   </Popover>
 </template>
